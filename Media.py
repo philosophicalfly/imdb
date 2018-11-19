23c024b762fefa3a45639edb02a56a175132d368
Tiago Ceccon <ceccon.t@hotmail.com>
2018-11-19 00:48:27 -0200

    Changes tsv importing logic to use Media class, keeping the previous code for reference, and makes some minor refactoring on other places

--- Media.py
+++ Media.py
@@ -3,18 +3,22 @@ class Media:
 
     counter = 0
 
-    def __init__(self, type, pryTittle, oriTitle, isAdult, startYear, endYear, runtime, genres):
+    def __init__(self, tconst, type, pryTittle, oriTitle, isAdult, startYear, endYear, runtime, genres):
+        self.tconst = tconst
         self.type = type
         self.priTitle = pryTittle
         self.oriTitle = oriTitle
         self.isAdult = isAdult
         self.startYear = startYear
-        self.startYear = endYear
+        self.endYear = endYear
         self.runtime = runtime
         self.genres= genres
         Media.counter += 1
 
     def toString(self):
         return 'type '+self.type + '\npriTitle  '+self.priTitle + '\npriTitle ' + self.priTitle + '\nisAdult ' +self.isAdult + '\nstartYear ' +self.startYear + '\nendYear ' +self.endYear + '\nruntime '+self.runtime +'\ngenres '+ self.genres
+
+    def toCsvRow(self):
+        return str(str([self.tconst, self.type, self.priTitle, self.oriTitle, self.isAdult, self.startYear, self.endYear, self.runtime, self.genres]) + '\n')
 
 
